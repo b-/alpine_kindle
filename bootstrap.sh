8078d5ce4e829bd98fc547c46b16212834f271d9
bri <bri@vm>
2024-07-14 05:32:46 +0000

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+if [[ ${UID} != 0 ]] ; then
+    echo "re-executing with sudo"
+    sudo "$0" "$@"
+else
+    # we are root!
+    apt-get update
+    apt-get upgrade -y
+    apt-get install just
+    just install_build_deps
+fi
